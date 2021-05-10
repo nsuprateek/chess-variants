8bac19d991b6d024caf5b70d649af8fe1bbd2be7
Suprateek Nag <tipunag12@gmail.com>
2021-05-10 21:24:10 +0200

    var to let, added semicolon

--- chessboard-variant.js
+++ chessboard-variant.js
@@ -1,102 +1,106 @@
-var board = null
-var game = new Chess()
-var $status = $('#status')
-var $fen = $('#fen')
-var $pgn = $('#pgn')
+let board = null
+let game = new Chess()
+let $status = $('#status')
+let $fen = $('#fen')
+let $pgn = $('#pgn')
 
 // ---------------- SELECT PROMOTION -----------------------------------
-var promotion = null;
+let promotion = null;
 $('#queen').on('click', () => {
   console.log("queen");
   promotion = 'q';
-})
+});
+
 $('#rook').on('click', () => {
   console.log("rook");
   promotion = 'r';
-})
+});
+
 $('#bishop').on('click', () => {
   console.log("bishop");
   promotion = 'b';
-})
+});
 
 $('#knight').on('click', () => {
   console.log("knight");
   promotion = 'n';
-})
+});
 // --------------------------------------------------------------------
 
 function onDragStart (source, piece, position, orientation) {
   // do not pick up pieces if the game is over
-  if (game.game_over()) return false
+  if (game.game_over()) return false;
 
   // only pick up pieces for the side to move
   if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
       (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-    return false
+    return false;
   }
 }
 
 function onDrop (source, target) {
   // see if the move is legal
-  var move = game.move({
+  let move = game.move({
     from: source,
     to: target,
-    promotion: window.promotion // NOTE: always promote to a queen for example simplicity
-  })
+    promotion: promotion // global variable promotion
+  });
 
   // move.promotion = window.prom
   // illegal move
-  if (move === null) return 'snapback'
+  if (move === null) return 'snapback';
 
-  updateStatus()
+  updateStatus();
 }
 
 // update the board position after the piece snap
 // for castling, en passant, pawn promotion
 function onSnapEnd () {
-  board.position(game.fen())
+  board.position(game.fen());
 }
 
 function updateStatus () {
-  var status = ''
+  let status = '';
 
-  var moveColor = 'White'
+  let moveColor = 'White';
   if (game.turn() === 'b') {
-    moveColor = 'Black'
+    moveColor = 'Black';
   }
 
   // checkmate?
   if (game.in_checkmate()) {
-    status = 'Game over, ' + moveColor + ' is in checkmate.'
+    status = 'Game over, ' + moveColor + ' is in checkmate.';
   }
 
   // draw?
   else if (game.in_draw()) {
-    status = 'Game over, drawn position'
+    status = 'Game over, drawn position';
   }
 
   // game still on
   else {
-    status = moveColor + ' to move'
+    status = moveColor + ' to move';
 
     // check?
     if (game.in_check()) {
-      status += ', ' + moveColor + ' is in check'
+      status += ', ' + moveColor + ' is in check';
     }
   }
 
-  $status.html(status)
-  $fen.html(game.fen())
-  $pgn.html(game.pgn())
+  $status.html(status);
+  $fen.html(game.fen());
+  $pgn.html(game.pgn());
+  console.log(game.board()[1][4]);
 }
 
-var config = {
+let config = {
   draggable: true,
   position: 'start',
   onDragStart: onDragStart,
   onDrop: onDrop,
-  onSnapEnd: onSnapEnd
-}
-board = Chessboard('myBoard', config)
+  onSnapEnd: onSnapEnd,
+  // orientation: 'black'
+};
+board = Chessboard('myBoard', config);
 
-updateStatus()
+updateStatus();
